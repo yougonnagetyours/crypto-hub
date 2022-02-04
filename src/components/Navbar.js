70d40279a9f43c13eba068f053102f0bb87cc3da
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-02-04 14:38:17 +0100

    creating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MonevCollectOutlined, Bulboutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='nav-container'>
+      <div className='logo-container'>
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">CryptoHub</Link>
+        </Typography.Title>
+        { /* <Button className= "menu-control-container">
+          /Button> */}
+      </div>
     </div>
   );
 };
