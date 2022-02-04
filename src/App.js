0dd114af7fc4bcdfa4557c6ddb2f6459a8063400
yougonnagetyours <michal.potoczny.92@gmail.com>
2022-02-04 12:52:50 +0100

    config app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Lavout, Typography, Space } from 'antd';
 
 const App = () => {
   return (
-    <div>
-      CryptoHub
+    <div className="app">
+      <div className="navbar">
+
+      </div>
+       <div className="main">
+         
+      </div>
+      <div className="footer">
+         
+      </div>
     </div>
   );
 };
